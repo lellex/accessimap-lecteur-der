6f8960e5c921fd63f7adf373d158297eedbcc6bc
Alexandra JANIN <contact.ajanin@gmail.com>
2017-04-11 16:05:53 +0200

    Setting up nw project

---
+++ nw/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var webspeechapi, vibrateWebApi, DerReader;
+
+webspeechapi = require('./../../tts.webapi/tts.webapi.js');
+vibrateWebApi = require('./../../vibrate.webapi/vibrate.webapi.js');
+DerReader = require('der-reader');
+
+DerReader.init({
+  container: 'der-reader',
+  derFile: null,
+  tts: webspeechapi,
+  vibrate: vibrateWebApi,
+  defaultMode: 0,
+  format: 'A4',
+  exit: function() {}
+});
