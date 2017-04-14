29d5281e916b5da1d4ea5b48a0afa6735904695d
Lellex <contact.ajanin@gmail.com>
2017-04-14 12:31:33 +0200

    travis conf

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
-# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
+echo "Starting deployment"
+echo "Target: gh-pages branch"
 
-# config
-git config --global user.email "travis@nobody.org"
-git config --global user.name "Travis CI"
+DIST_DIRECTORY="modules/der-reader/dist/"
+CURRENT_COMMIT=`git rev-parse HEAD`
+ORIGIN_URL=`git config --get remote.origin.url`
+ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
 
-# deploy
-cd modules/der-reader/dist
-ls
-git status
-git init
-git add .
-git commit -m "Deploy to Github Pages"
-git status
-git push --force --quiet "https://${GITHUB_API_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+cp .gitignore $DIST_DIRECTORY || exit 1
+
+echo "Checking out gh-pages branch"
+git checkout -B gh-pages || exit 1
+
+echo "Removing old static content"
+git rm -rf . || exit 1
+
+echo "Copying dist content to root"
+cp -r $DIST_DIRECTORY/* . || exit 1
+cp $DIST_DIRECTORY/.gitignore . || exit 1
+
+echo "Pushing new content to $ORIGIN_URL"
+git config user.name "YOUR-GITHUB-USERNAME" || exit 1
+git config user.email "YOUR-GITHUB-EMAIL" || exit 1
+
+git add -A . || exit 1
+git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
+git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
+
+echo "Cleaning up temp files"
+rm -Rf $DIST_DIRECTORY
+
+echo "Deployed successfully."
+exit 0
